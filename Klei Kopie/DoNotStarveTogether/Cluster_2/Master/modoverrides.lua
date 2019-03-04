dd261ddee83a1a73b0edef62243365f1300aa020
Pascal Borel <pascal.borel@gmx.net>
2019-03-04 07:46:34 +0100

    Again, look away for a sec

---
+++ Klei Kopie/DoNotStarveTogether/Cluster_2/Master/modoverrides.lua
@@ -0,0 +1,17 @@
+return {
+  ["workshop-1207269058"]={
+    configuration_options={
+      ddon=true,
+      hbcolor="dynamic",
+      hblength=10,
+      hbpos=1,
+      hbstyle="heart",
+      value=true 
+    },
+    enabled=true 
+  },
+  ["workshop-421029894"]={
+    configuration_options={ key_disorganized=122, key_paranoid=99, key_psychotic=120 },
+    enabled=true 
+  } 
+}
